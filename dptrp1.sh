648cc59b85634937a9c890aad70784ef32908b73
Cristóbal Tapia Camú <crtapia@gmail.com>
2021-04-10 13:27:25 +0200

    feat: a hacky way to directly copy the original pdf

--- dptrp1.sh
+++ dptrp1.sh
@@ -24,6 +24,32 @@ sanitized_jobtitle="$(echo ${jobtitle} | awk -F '/' '{print $NF}' | \
 outname=/tmp/${printtime}_${sanitized_jobtitle}
 docname=${printtime}_${sanitized_jobtitle}
 
+# Hacky way to determine whether the original pdf file should be
+# transfered to the DPT-RP1 or the file ceated by CUPS. If only
+# some pages of the file should be printed, then the CUPS-generated
+# file is used
+function copy_original_or_not() {
+  # Confirm that there is an original file as pdf
+  if test -f "${jobtitle}"; then
+    # Compare extension
+    extension=$(basename ${jobtitle} | awk -F '.' '{print $NF}')
+    if [ "$extension" = "pdf" ]; then
+      # Compare number of pages
+      npages_a=$(pdfinfo ${jobtitle} | awk '/^Pages:/ {print $2}')
+      npages_b=$(pdfinfo ${jobfile} | awk '/^Pages:/ {print $2}')
+      if [ "${npages_a}" = "${npages_b}" ]; then
+        echo 1
+      else
+        echo 0
+      fi
+    else
+      echo 0
+    fi
+  else
+    echo 0
+  fi
+}
+
 case ${#} in
     0)
         # this case is for "backend discovery mode"
@@ -46,7 +72,14 @@ case ${#} in
         ;;
 
     6)
-        cat ${6} > ${outname}
+        # A hacky way to just copy the original PDF if the original document is PDF
+        originalfile=$(copy_original_or_not)
+        if [ ${originalfile} = 1 ]; then
+          cp ${jobtitle} ${outname}
+        else
+          cat ${6} > ${outname}
+        fi
+
         ${dptrp1path} --addr=${DEVICEADDR} \
             --client=${DPT_ID} \
             --key=${DPT_KEY} \
