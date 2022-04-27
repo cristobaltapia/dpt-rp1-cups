12278c7cb6461e629f7a9b8e82d1f7121823d08a
Cristóbal Tapia Camú <crtapia@gmail.com>
2022-04-27 09:54:19 +0200

    feat: add icon to print notification

--- dptrp1.sh
+++ dptrp1.sh
@@ -72,6 +72,7 @@ case ${#} in
 
         # Send notification to user
         sudo -u ${cupsuser} DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/${useruuid}/bus notify-send.sh 'DPT-RP1' "${docname} Printed!" \
+          --icon=input-tablet \
           --action="Show in device":"dptrp1 --addr=${DEVICEADDR} --client=${DPT_ID} --key=${DPT_KEY} display-document Document/Printed/${docname}"
         ;;
 
@@ -93,6 +94,7 @@ case ${#} in
 
         # Send notification to user
         sudo -u ${cupsuser} DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/${useruuid}/bus notify-send.sh 'DPT-RP1' "${docname} Printed!" \
+          --icon=input-tablet \
           --action="Show in device":"dptrp1 --addr=${DEVICEADDR} --client=${DPT_ID} --key=${DPT_KEY} display-document Document/Printed/${docname}"
         ;;
 esac
